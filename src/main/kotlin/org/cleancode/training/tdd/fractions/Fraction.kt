6668b9f4b9a5fb752a4c875d64d3dce669e420ff
@lilo$oft <fellahi.ali@gmail.com>
2019-01-20 00:43:00 +0100

    Implements Addition when dinomenators are different (Without simplification)

--- src/main/kotlin/org/cleancode/training/tdd/fractions/Fraction.kt
+++ src/main/kotlin/org/cleancode/training/tdd/fractions/Fraction.kt
@@ -3,9 +3,18 @@ package org.cleancode.training.tdd.fractions
 data class Fraction(val numerator: Int, val denominator: Int = 1) {
 
     fun add(other: Fraction): Fraction {
-        val numerator = this.numerator + other.numerator
-        val denominator = this.denominator
-        return Fraction(numerator, denominator)
+        var rNumerator = 0
+        var rDenominator = 0
+
+        if (this.denominator == other.denominator) {
+            rNumerator = this.numerator + other.numerator
+            rDenominator = this.denominator
+        } else {
+            // calc common denominator
+            rNumerator = this.numerator * other.denominator + other.numerator * this.denominator
+            rDenominator = this.denominator * other.denominator
+        }
+        return Fraction(rNumerator, rDenominator)
     }
 
 }
--- src/test/kotlin/org/cleancode/training/tdd/fractions/FractionTest.kt
+++ src/test/kotlin/org/cleancode/training/tdd/fractions/FractionTest.kt
@@ -4,10 +4,6 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 internal class FractionTest {
-//    @Test fun addFractions() {
-//        assertEquals(Fraction(numerator = 47, denominator = 15),
-//                Fraction(7, 3).add(Fraction(4, 5)))
-//    }
 
     @Test fun additionWhenDenominatorIsOne() {
         assertEquals(Fraction(7),
@@ -17,6 +13,11 @@ internal class FractionTest {
     @Test fun additionWhenDenominatorsAreEqual() {
         assertEquals(Fraction(5, 7),
                 Fraction(3, 7).add(Fraction(2, 7)))
+    }
+
+    @Test fun additionWhenDenominatorsAreDifferent() {
+        assertEquals(Fraction(numerator = 47, denominator = 15),
+                Fraction(7, 3).add(Fraction(4, 5)))
     }
 
 }
