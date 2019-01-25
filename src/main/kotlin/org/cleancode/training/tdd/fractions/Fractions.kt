564346b13238fe1b5581504bb287d53b29b98b8f
@lilo$oft <fellahi.ali@gmail.com>
2019-01-25 19:00:54 +0100

    Refactoring & cleaning add() method by combining the two cases in one general case

--- src/main/kotlin/org/cleancode/training/tdd/fractions/Fractions.kt
+++ src/main/kotlin/org/cleancode/training/tdd/fractions/Fractions.kt
@@ -1,7 +1,5 @@
 package org.cleancode.training.tdd.fractions
 
-import kotlin.math.abs
-
 infix fun Int.over(denominator: Int) = Fraction(this, denominator)
 
 data class Fraction(var numerator: Int, var denominator: Int = 1) {
@@ -11,29 +9,9 @@ data class Fraction(var numerator: Int, var denominator: Int = 1) {
     }
 
     fun add(other: Fraction): Fraction {
-        val rNumerator: Int
-        val rDenominator: Int
-
-        if (this.denominator == other.denominator) {
-            rNumerator = this.numerator + other.numerator
-            rDenominator = this.denominator
-        } else {
-            // unify the denominator
-            rNumerator = this.numerator * other.denominator + other.numerator * this.denominator
-            rDenominator = this.denominator * other.denominator
-        }
+        val rNumerator = this.numerator * other.denominator + other.numerator * this.denominator
+        val rDenominator = this.denominator * other.denominator
         return Fraction(rNumerator, rDenominator)
     }
 
-    private fun reduce() {
-        val gcd = gcd(numerator, denominator)
-        val denominatorSign = if (denominator < 0 ) -1 else  1
-        numerator /= gcd * denominatorSign
-        denominator /= gcd * denominatorSign
-    }
-
-}
-
-fun gcd(a: Int, b: Int): Int {
-    return if (b == 0) abs(a) else gcd(b, a % b)
 }
